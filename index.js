cd0410ec4df2363f47499666b1160401067ed374
Suglo Suleman Rufiatu <101270194+rufia-suleman@users.noreply.github.com>
2022-04-22 07:34:13 -0700

    added error message

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const usersRoute = require("./routes/usersRoutes")
 const path = require("path")
 const logger = require("./middlewares/user.logger")
 const booksRoute = require("./routes/booksRoute")
+require("dotenv").config()
+const connectDB = require("./config/connectDB")
 
+connectDB( )
 const app = express()
 
 //middlewares
 app.use(express.json());
 app.use(logger)
-app.use(usersRoute)
+app.use("/users", usersRoute)
 app.use(booksRoute)
 
 //home route
@@ -28,5 +31,5 @@ const PORT = 4000;
 
 
 app.listen(PORT, ()=>{
-    console.log("SERVER IS UP")
+    console.log("Congratulation")
 })
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,7 +4,8 @@ const auth = require("../middlewares/user.auth")
 const {getUsers, createUser, getUser} = require("../controllers/usersController")
 
 const router = Router();
-router.route("/users").get(auth, getUsers).post(createUser).get(getUser)
+router.route("/users").get(auth, getUsers)
+router.post("/register", createUser)
 router.get("/users/:id", auth, getUser)
 
 
